56c0d6c7d21da6bfe90b42fcc02bbc6b8fd483ac
Dicky Kurniawan <89238386+kurniawandicky02@users.noreply.github.com>
2022-08-14 12:41:33 +0700

    Add files via upload

---
+++ views/_partials/footer.php
@@ -0,0 +1,8 @@
+<footer class="sticky-footer bg-white">
+	<div class="container my-auto">
+	    <div class="copyright text-center my-auto">
+	    <span>
+	    <a href="https://github.com/kurniawandicky02" target="_blank">Dicky Kurniawan</a> &copy; Aplikasi Rental Mobil <?= date('Y') ?></span>
+	    </div>
+	</div>
+</footer>
